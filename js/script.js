0041f591e5b91397ac1ec57d454a56b715d60f5b
Vang Khanh Khuyen <trinhnganquan@gmail.com>
2026-01-13 09:33:18 +0700

    display question successfully

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ var currentQuiz = {
     correct_answer: '',
     explanation: ''
 }
+let topics = [];
 var startScreen = document.getElementById('start-screen');
 var quizScreen = document.getElementById('quiz-screen');
-
+var topicId = 1;
 // ================FETCH CSDL==================
 
 const DB_URL = '../db/db.json';
@@ -17,9 +18,12 @@ var data = fetch(DB_URL)
     return response.json();
 })
 .then(function(data){
-    const topics = data.topics;
+    topics = data.topics;
     console.log(topics);
     displayTopics(topics);
+    console.log(topicId);
+
+
     
 })
 .catch(error =>{
@@ -36,25 +40,44 @@ function displayTopics(topics){
     });
     topicSelection.innerHTML = selectionList;
 }
-var topicId;
+
 var startBtn = document.getElementById('start-btn');
-topicSelection.addEventListener('change',(e)=>{
-    e.stopPropagation();
-    topicId = topicSelection.value;
-});
+
+
 
 startBtn.addEventListener('click',(e)=>{
     e.stopPropagation();
     if (topicId !== 0){
         startScreen.classList.replace('active', 'hidden');
         quizScreen.classList.replace('hidden', 'active');
+        renderQuestion(topics[topicId-1]);
     };
 });
 
+var quizTopic = document.getElementById('quiz-topic');
+var questionText = document.getElementById('question-text');
+var currentQuestionCounter = document.getElementById('current-ques');
+var optionBtns = document.querySelectorAll('.option-btn');
+
+
 var questionNo = 0;
 function renderQuestion(topic){
+    console.log(topic);
+    var topicName = topic.name;
+    quizTopic.textContent = "TOPIC: "+topicName;
+
     var questions = topic.questions;
     currentQuiz = questions[questionNo];
+
+    currentQuestionCounter.textContent = questionNo;
+    questionText.textContent = currentQuiz.question;
     console.log(currentQuiz);
+
+    optionBtns.forEach(btn => {
+        const label = btn.querySelector('.option-label').textContent;
+        const optionText = btn.querySelector('.option-text');
+        optionText.textContent = currentQuiz.options[label];
+    })
+    
 }
 //can lam render topic dung chu de select
